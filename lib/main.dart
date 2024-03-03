51d88a16d464d2d55b3dee4b9c9708cb234bb950
abdenour hachrouf <abdouopt5@gmail.com>
2024-03-03 14:18:11 +0100

    after add folders

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:counter_getx/mvc/modules/home/view.dart';
-import 'package:counter_getx/post/Post_Screen.dart';
 import 'package:flutter/material.dart';
+import 'package:counter_getx/post/Post_Screen.dart';
 import 'package:counter_getx/mvc/view/Home_Screen.dart';
-import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,14 +12,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp(
       title: 'Flutter Counter App',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomeQuotesScreen(),
+      home: HomeScreen(),
     );
   }
 }
